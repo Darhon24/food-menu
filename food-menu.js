0cd88717c4c6cccf0264f456ef97f9a5b2e03ada
DaRhon DaSilva <ddasilva@tb.yearup.org>
2022-11-07 13:21:17 -0500

    Added html and javascript

---
+++ food-menu.js
@@ -0,0 +1,8 @@
+let selectMenu = document.getElementById('catagories');
+    console.log(the select menu is ${selectMenu});
+
+    selectMenu.onchange = otherMenuList;
+
+    function otherMenuList() {
+        let itemList = document.getElementById('dropDownTwo')
+    }
